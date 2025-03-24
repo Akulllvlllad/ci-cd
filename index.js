eb02a70299bb36592cb516d5d4a1f3a77414013c
vwinterdev@gmail.com <Akulllvlllad@gmail.com>
2025-03-24 15:32:32 +0700

    add piplane

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require("http");
+
+const server = http.createServer(function (request, response) {
+  response.end("Hello METANIT.COM!");
+});
+server.listen(80, function () {
+  console.log("Сервер запущен по адресу http://localhost:3000");
+});
